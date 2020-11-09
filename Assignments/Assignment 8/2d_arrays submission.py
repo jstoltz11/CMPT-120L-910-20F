427b242e77527a06ed05343e0d2be4f25b4e236a
jstoltz11 <70299227+jstoltz11@users.noreply.github.com>
2020-11-09 17:17:20 -0500

    Assignment 8

---
+++ Assignments/Assignment 8/2d_arrays submission.py
@@ -0,0 +1,20 @@
+def sum_2d_array(two_d_array):
+    output = []
+    for lst in two_d_array:
+        output.append(sum(lst))
+    return output
+    pass
+
+if __name__ == "__main__":
+    two_d_array = [
+        [2, 6, 7, 98, 3, 434, 2, 4, 2],
+        [-12, 3, 454, 6778, 234, -999, 2543, -2323],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        [],
+        [1000000000000000],
+        [1],
+        [0]
+        ]
+    answers = sum_2d_array(two_d_array)
+    
+    print(answers)
