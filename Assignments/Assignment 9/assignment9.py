c31d06cafafe9a673ae9f4c86b416a498eec8dd0
jstoltz11 <70299227+jstoltz11@users.noreply.github.com>
2020-11-17 18:56:45 -0500

    Assignment 9 Submission

---
+++ Assignments/Assignment 9/assignment9.py
@@ -0,0 +1,11 @@
+from Calculator import Calculator
+
+calc = Calculator()
+
+print(calc.addition(8, 2))
+print(calc.subtraction(10, 3))
+print(calc.division(24, 4))
+print(calc.multiplication(11, 4))
+print(calc.exponent(4, 3))
+print(calc.square_root(25))
+print(calc.negate(-9))
