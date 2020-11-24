c0da37d86f0250053a7b8a9d0fe201308d456f05
jstoltz11 <70299227+jstoltz11@users.noreply.github.com>
2020-11-24 18:46:18 -0500

    Lab 9 Submission

--- SemesterProject/app.py
+++ SemesterProject/app.py
@@ -1,11 +1,29 @@
 from flask import Flask
 from flask import render_template
+import logging
 
 app = Flask(__name__)
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 @app.route('/')
 def index():
+    app.logger.info('User is on the home page.')
     return render_template('index.html')
 
-hell0 = hello
+@app.route('/1')
+def page1():
+    app.logger.info('User is on page 1.')
+    return render_template('page1.html')
+
+@app.route('/2')
+def page2():
+    app.logger.info('User is on page 2.')
+    return render_template('page2.html')
+
+@app.route('/3')
+def page3():
+    app.logger.info('User is on page 3.')
+    return render_template('page3.html')
